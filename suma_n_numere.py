92ed6a6381be070c3407124df44a07fcf0ecfe19
CiprianRad <89615038+CiprianRad@users.noreply.github.com>
2024-10-10 23:24:44 +0300

    Update suma_n_numere.py

--- suma_n_numere.py
+++ suma_n_numere.py
@@ -1,3 +1,4 @@
+#this function calculates the sum of the first n numbers using a loop
 def sum_of_natural_numbers(n):
     total = 0
     for i in range (0, n):
@@ -5,16 +6,16 @@ def sum_of_natural_numbers(n):
     return total
 
 
-
+# this function allows us to get a valid input from the user
 def get_natural_number_input():
-    while True:
-        try:
+    while True:  # while True will run this function until we get the valid input from the user
+        try:  # the try block will probe if what the user inputed will lead to an error or not 
             n = int(input("Enter a natural number: "))
             if n > 0:
                 return n
-            else:
+            else: # this is how we handled the error or exception in case n is negative
                 print("Please enter a number greater than zero: ")
-        except ValueError:
+        except ValueError: #we used a value error keyword to make sure the user does not input strings or other charaters
             print("Invalid input. Please enter a valid natural number ")
 
 
@@ -27,5 +28,6 @@ def main():
     print("Sum of the first n numbers is", sum_of_natural_numbers(n))
 
 main()
+
 
 
